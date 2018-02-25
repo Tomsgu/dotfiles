16908ceae553f78a14aeb146abcb301f060d2c35
Tomas <tomasjakll@gmail.com>
2018-02-25 11:45:51 +0100

    Add an installation script.

---
+++ install.sh
@@ -0,0 +1,171 @@
+#!/bin/bash
+# This script was partly copied from  https://github.com/nicksp/dotfiles/blob/master/setup.sh
+
+#----- Utils
+answer_is_yes() {
+    [[ "$REPLY" =~ ^[Yy]$ ]] \
+        && return 0 \
+        || return 1
+}
+
+ask() {
+    print_question "$1"
+    read
+}
+
+ask_for_confirmation() {
+    print_question "$1 (y/n) "
+    read -n 1
+    printf "\n"
+}
+
+ask_for_sudo() {
+
+    # Ask for the administrator password upfront
+    sudo -v
+
+    # Update existing `sudo` time stamp until this script has finished
+    # https://gist.github.com/cowboy/3118588
+    while true; do
+        sudo -n true
+        sleep 60
+        kill -0 "$$" || exit
+    done &> /dev/null &
+
+}
+
+cmd_exists() {
+    [ -x "$(command -v "$1")" ] \
+        && printf 0 \
+        || printf 1
+}
+
+execute() {
+    $1 &> /dev/null
+    print_result $? "${2:-$1}"
+}
+
+get_answer() {
+    printf "$REPLY"
+}
+
+get_os() {
+
+    declare -r OS_NAME="$(uname -s)"
+    local os=""
+
+    if [ "$OS_NAME" == "Darwin" ]; then
+        os="osx"
+    elif [ "$OS_NAME" == "Linux" ] && [ -e "/etc/lsb-release" ]; then
+        os="ubuntu"
+    fi
+
+    printf "%s" "$os"
+
+}
+
+is_git_repository() {
+    [ "$(git rev-parse &>/dev/null; printf $?)" -eq 0 ] \
+        && return 0 \
+        || return 1
+}
+
+mkd() {
+    if [ -n "$1" ]; then
+        if [ -e "$1" ]; then
+            if [ ! -d "$1" ]; then
+                print_error "$1 - a file with the same name already exists!"
+            else
+                print_success "$1"
+            fi
+        else
+            execute "mkdir -p $1" "$1"
+        fi
+    fi
+}
+
+print_error() {
+    # Print output in red
+    printf "\e[0;31m  [✖] $1 $2\e[0m\n"
+}
+
+print_info() {
+    # Print output in purple
+    printf "\n\e[0;35m $1\e[0m\n\n"
+}
+
+print_question() {
+    # Print output in yellow
+    printf "\e[0;33m  [?] $1\e[0m"
+}
+
+print_result() {
+    [ $1 -eq 0 ] \
+        && print_success "$2" \
+        || print_error "$2"
+
+    [ "$3" == "true" ] && [ $1 -ne 0 ] \
+        && exit
+}
+
+print_success() {
+    # Print output in green
+    printf "\e[0;32m  [✔] $1\e[0m\n"
+}
+
+# Warn user this script will overwrite current dotfiles
+while true; do
+    read -p "Warning: this will overwrite your current dotfiles. Continue? [y/n] " yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+#----- Files to symlink
+declare -a FILES_TO_SYMLINK=(
+'shell/zshrc'
+
+'vim/vimrc'
+
+'idea/ideavimrc'
+
+'tmux/tmux.conf'
+
+'git/gitconfig'
+'git/gitignore_global'
+)
+
+# FILES_TO_SYMLINK="$FILES_TO_SYMLINK .vim bin" # add in vim and the binaries
+
+# Move any existing dotfiles in homedir to dotfiles_old directory, 
+# then create symlinks from the homedir to any files in the ~/dotfiles 
+# directory specified in $files
+
+echo "Moving any existing dotfiles from ~ to $dir_backup"
+for i in ${FILES_TO_SYMLINK[@]}; do
+    mv ~/.${i##*/} ~/dotfiles_old/
+done
+
+for i in ${FILES_TO_SYMLINK[@]}; do
+
+    sourceFile="$(pwd)/$i"
+    targetFile="$HOME/.$(printf "%s" "$i" | sed "s/.*\/\(.*\)/\1/g")"
+
+    if [ ! -e "$targetFile" ]; then
+        execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
+    elif [ "$(readlink "$targetFile")" == "$sourceFile" ]; then
+        print_success "$targetFile → $sourceFile"
+    else
+        ask_for_confirmation "'$targetFile' already exists, do you want to overwrite it?"
+        if answer_is_yes; then
+            rm -rf "$targetFile"
+            execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
+        else
+            print_error "$targetFile → $sourceFile"
+        fi
+    fi
+
+done
+
+unset FILES_TO_SYMLINK
